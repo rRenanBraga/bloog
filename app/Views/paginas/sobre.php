fb716063d55044a55e27e6ac8f3e34a80ae1acbc
rRenanBraga <104376037+rRenanBraga@users.noreply.github.com>
2023-08-13 17:41:09 -0300

    co

---
+++ app/Views/paginas/sobre.php
@@ -0,0 +1,19 @@
+<div class="container my-3">
+    <div class="card">
+        <div class="card-body">
+            <h1 class="my-3"><?= $dados['tituloPagina'] ?></h1>
+
+            <p><b>O que você aprenderá?</b></p>
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Aprendendo PHP</li>
+
+                <li class="list-group-item">Compreendendo o padrão MVC</li>
+                <li class="list-group-item">Autenticação de usuário completa usando OOP e PDO</li>
+                <li class="list-group-item">Validar formulários</li>
+               
+            </ul>
+        </div>
+    </div>
+
+</div>
